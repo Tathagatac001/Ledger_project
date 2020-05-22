057f73214b6773fef331fb5fb0e8d413ab76ee4e
EC2 Default User <ec2-user@ip-172-31-44-144.us-east-2.compute.internal>
2020-05-22 22:33:18 +0000

    test

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -3,7 +3,7 @@ git remote add heroku https://github.com/Tathagatac001/Ledger_project.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+#sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 HEROKU_LOGIN='tathag_678@yahoo.co.in'
 HEROKU_API_KEY='94eba20b-17ce-4846-8a5a-0bc3fe81dadb'
 cat > ~/.netrc << EOF
