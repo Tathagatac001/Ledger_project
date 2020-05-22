5e864a1350c653e4462b58843948da3a97788a67
EC2 Default User <ec2-user@ip-172-31-44-144.us-east-2.compute.internal>
2020-05-22 21:29:57 +0000

    test

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+!/bin/bash
 git remote add heroku https://git.heroku.com/circleci-demo-python-flask.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
 sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
-
+HEROKU_LOGIN='tathag_678@yahoo.co.in'
+HEROKU_API_KEY='Tatha@123'
 cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
